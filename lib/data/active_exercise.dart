46067fe19fd6da5c7fa6022478c9e53900b9ea53
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-07-16 21:43:55 -0700

    added active exercise class

---
+++ lib/data/active_exercise.dart
@@ -0,0 +1,35 @@
+import "package:flutter_application_attempt_2/data/repped_set.dart";
+import "package:flutter_application_attempt_2/data/timed_set.dart";
+
+import 'workout_set.dart';
+import "enums_exercise_types.dart";
+
+class ActiveExercise {
+  String name;
+  List<Enum> primaryMusclesWorked;
+  List<Enum> secondaryMusclesWorked;
+  Enum type;
+  Enum statType;
+  double? currentPR;
+
+  List<WorkoutSet> sets = [];
+
+  ActiveExercise({
+    required this.name,
+    required this.primaryMusclesWorked,
+    required this.secondaryMusclesWorked,
+    required this.type,
+    required this.statType,
+    required currentPR
+  });
+
+  addSet(){
+    if (statType == ExerciseStatType.reps){
+      sets.add(new ReppedSet());
+    }
+    else if (statType == ExerciseStatType.timed){
+      sets.add(new TimedSet());
+    }
+  }
+
+}
