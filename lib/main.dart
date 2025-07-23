41c51623a5ef982c9032fde88d3c1338a929c60d
Lucas Slunt <100328965+LucasSlunt@users.noreply.github.com>
2025-07-22 20:42:38 -0700

    basic stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'data/repped_set.dart';
+import 'data/timed_set.dart';
+import 'data/workout_set.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +24,6 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -62,9 +64,61 @@ class CurrentWorkoutScreen extends StatelessWidget{
             color: Color.fromARGB(255, Random().nextInt(255), 0, 0),
             height: 500,
             width: 500,
+            //child: Text("Page $index"),
+            //child: ExerciseWidget("$index").buildName(context)
+            child: ReppedSetWidget(name: "exercise 1"),
           );
         }
       ),
+    );
+  }
+}
+
+class ExerciseWidget {
+  final String name;
+    
+  ExerciseWidget(this.name);
+
+  Widget buildName(BuildContext context){
+
+    return Text(name, style: Theme.of(context).textTheme.headlineLarge);
+  }
+}
+/*
+class ReppedSetWidget {
+
+  ReppedSet set = ReppedSet();
+
+  Widget buildNumberOfReps(BuildContext context){
+    return ElevatedButton(
+      onPressed: () => {
+
+      }, 
+      child: Text("Number of Reps: "));
+  }
+}*/
+
+class ReppedSetWidget extends StatefulWidget{
+  final String name;
+
+  const ReppedSetWidget({super.key, required this.name});
+
+  @override ReppedSetState createState() => ReppedSetState();
+}
+
+class ReppedSetState extends State<ReppedSetWidget> {
+  int numOfReps = 0;
+
+  @override
+  Widget build(BuildContext context){
+    //return Text('${widget.name}: $numOfReps');
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          numOfReps++;
+        });
+      },
+      child: Text('${widget.name}: $numOfReps'),
     );
   }
 }
